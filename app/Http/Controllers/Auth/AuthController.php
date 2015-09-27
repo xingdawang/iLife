1ac54cb1d11308c5011754c07e6b6d30bd817396
Xingda Wang <wxd598113636@gmail.com>
2015-09-27 17:02:45 +0100

    After successfully logged in, redirect to / page

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -23,10 +23,10 @@ class AuthController extends Controller
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
+    protected $redirectPath = '/';
+
     /**
      * Create a new authentication controller instance.
-     *
-     * @return void
      */
     public function __construct()
     {
